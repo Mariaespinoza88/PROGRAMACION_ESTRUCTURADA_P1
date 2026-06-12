920f56bac5fc376e57bb1fb465071355f0d5315b
Mariaespinoza88 <maria_3142250006@utd.edu.mx>
2026-06-11 23:27:57 -0600

    Delete P1 directory

--- P1/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos,modulo_paquete
-
-modulos.borrarpantalla()
-nom,ape=modulos.funcion2()
-edad=modulo_paquete.solicitaredad()
-
-print(f"Nombre: {nom}\nApellidos:{ape}\nEdad:{edad}")
-
