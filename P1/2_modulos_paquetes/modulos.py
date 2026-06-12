17e62b2bd9c8781442bc237955d77b64e0a7eec5
Mariaespinoza88 <maria_3142250006@utd.edu.mx>
2026-06-11 23:21:17 -0600

    Add files via upload

---
+++ P1/2_modulos_paquetes/modulos.py
@@ -0,0 +1,28 @@
+# Un módulo es simplemente un archivo con extensión .py que contiene código de Python (funciones, clases, variables, etc.).
+#si no no es un modulo
+
+def borrarpantalla():
+ print("\033c")
+def funcion1():
+    nombre=input("Nombre: ").upper().strip()
+    apellido=input("Apellidos: ").upper().strip()
+    print(f"El nombre del alumno es: {nombre} {apellido}") 
+    funcion1()
+def funcion3(nom,ape):
+    nombre=nom
+    apellido=ape
+    print(f"El nombre del alumno es: {nombre} {apellido}") 
+
+
+ #2.- Funcion que no recibe parametros y regresa valor
+def funcion2():
+    nombre=input("Nombre: ").upper().strip()
+    apellido=input("Apellidos: ").upper().strip()
+    return nombre, apellido
+
+ #4.- Funcion que recibe parametros y regresa valor
+def funcion4(nom,ape):
+    nombre=nom
+    apellido=ape
+    return nombre,apellido
+
---
+++ P1/2_modulos_paquetes/utilizar_paquetes.py
@@ -0,0 +1,8 @@
+from paquete1 import modulos,modulo_paquete
+
+modulos.borrarpantalla()
+nom,ape=modulos.funcion2()
+edad=modulo_paquete.solicitaredad()
+
+print(f"Nombre: {nom}\nApellidos:{ape}\nEdad:{edad}")
+
